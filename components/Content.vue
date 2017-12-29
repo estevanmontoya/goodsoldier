ade2dba22c159366ad64a36627d330934e3a1eac
Estevan Montoya <montoya.estevan@gmail.com>
2017-12-29 02:29:56 -0700

    Added FB link

--- components/Content.vue
+++ components/Content.vue
@@ -3,7 +3,11 @@
     <div class="container">
       <div class="audio">
         <h2 class="contentH2">Recent Episodes</h2>
-        <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/365190977&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=false"></iframe>
+        <iframe style="margin-bottom:50px;" width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/365190977&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=false"></iframe>
+        <p class="likeUs">Like our fanpage of Facebook to be encouraged with shareable social media graphics and quotes that are uplifting.</p>
+        <a href="https://www.facebook.com/GoodSoldier.co/" target="_blank">
+          <img src="../static/facebook-logo.jpg" alt="Like our fanpage on Facebook.">
+        </a>
       </div>
       <div class="side">
         <h2 class="contentH2">Sponsors</h2>
@@ -38,17 +42,28 @@ section {
   text-transform: uppercase;
   margin-bottom: 40px;
 }
-.side img {
-  /* max-width: 500px; */
+.likeUs {
+  font-family: 'Cardo', serif;
+  font-size: 16px;
+  font-style: italic;
+  margin-bottom: 20px;
+  -moz-transition: all .5s ease-out;-o-transition: all .5s ease-out;transition: all .5s ease-out;
+}
+img {
+  max-width: 430px;
   width: 100%;
   border: 2px solid (0,0,0,.75);
   box-shadow: 0px 8px 6px -3px rgba(0,0,0,.5);
   margin-bottom: 40px;
+  -moz-transition: all .5s ease-out;-o-transition: all .5s ease-out;transition: all .5s ease-out;
 }
 @media (min-width:768px) {
   .container {
     grid-template-columns: 2fr 1fr;
     padding: 30px 30px 100px;
+  }
+  .likeUs {
+    font-size: 30px;
   }
 }
 @media (min-width:1024px) {
