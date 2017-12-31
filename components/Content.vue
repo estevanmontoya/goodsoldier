b9f070901a9a927b8c71c513d23f511fef512a2d
Estevan Montoya <montoya.estevan@gmail.com>
2017-12-30 23:10:26 -0700

    Added iTunes link

--- components/Content.vue
+++ components/Content.vue
@@ -5,6 +5,9 @@
       <div class="audio">
         <iframe style="margin-bottom:50px;" width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/365190977&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=false"></iframe>
       </div>
+      <a href="https://itunes.apple.com/us/podcast/podcast-good-soldier-podcast-feed/id1321276329?mt=2" target="_blank">
+        <img class="itunes" src="../static/itunes.png" alt="Subscribe to our podcast on iTunes.">
+      </a>
       <div class="sponsors">
         <a href="http://simplechurchtools.co" target="_blank">
           <img src="../static/SCT.jpg" alt="Simple Church Tools. We are building a suite of tools to solve common church headaches and give pastors back time and sanity.">
@@ -40,8 +43,11 @@ section {
   grid-template-columns: 1fr;
   grid-gap: 10px;
 }
+.itunes {
+  margin-bottom: 50px;
+}
 img {
-  max-width: 300px;
+  max-width: 500px;
   width: 100%;
   border: 2px solid (0,0,0,.75);
   box-shadow: 0px 8px 6px -3px rgba(0,0,0,.25);
@@ -59,6 +65,9 @@ img:hover {
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-gap: 30px;
+  }
+  .itunes {
+    margin-bottom: 100px;
   }
 }
 @media (min-width:1024px) {
